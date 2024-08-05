5433068b94447059a63473146adbe775796354fa
Alperzkn <20475676+Alperzkn@users.noreply.github.com>
2024-08-05 15:55:31 +0300

    cards and icons positions fixed

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,17 +1,16 @@
 import "bootstrap-icons/font/bootstrap-icons.css";
-import "./card.module.css"
+import "./card.css";
 
 export function Card({ cardIcon, title, description }) {
   return (
-    
-      <div className="card bg-light border-0 h-100">
-        <div className="card-body text-center p-4 p-lg-5 pt-0 pt-lg-0">
-          <div className="feature bg-primary bg-gradient text-white rounded-3 mb-4 mt-n4 fs-2 customCard">
-            <i className={cardIcon}></i>
-          </div>
-          <h2 className="fs-4 fw-bold">{title}</h2>
-          <p className="mb-0">{description}</p>
+    <div className="card bg-light border-0 h-100">
+      <div className="card-body text-center p-4 p-lg-5 pt-0 pt-lg-0">
+        <div className="feature bg-primary bg-gradient text-white rounded-3 mb-4 mt-n4 fs-2">
+          <i className={cardIcon}></i>
         </div>
+        <h2 className="fs-4 fw-bold">{title}</h2>
+        <p className="mb-0">{description}</p>
       </div>
+    </div>
   );
 }
